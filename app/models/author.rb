f0c3e301ea8772a0607b8a4733546ef7809f0f74
Shayna Chhari <shaynachhari@gmail.com>
2023-11-06 20:29:07 +0530

    add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
       has_many :books
 
+      validates_associated :books
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,22 +1,42 @@
 class Student < ApplicationRecord
-    has_many :blogs
+    # has_many :blogs
+    # custom validation
+
+    validate :poolstack_email
+
+    private
+
+    def poolstack_email
+     unless email.end_with?('@poolstack')
+       errors.add(:email, 'must end with @poolstack')
+    end
+  end
+
     
-    has_one_attached :profile_image
-    #   person.profile_image.attach(io: File.open("/path/to/face.jpg"), filename: "shayna.jpeg", content_type: "image/jpeg")
+    # has_one_attached :profile_image
+    # person.profile_image.attach(io: File.open("/path/to/face.jpg"), filename: "shayna.jpeg", content_type: "image/jpeg")
  
 
 
+    # validates
+    # validates :name, presence: { strict: true }, length: {maximum: 14, minimum:5}
+    # validates :token, presence: true, uniqueness: true, strict: TokenGenerationException
+    # validates :name, inclusion: { in: %w(small medium large),
+    # message: "%{value} is not a valid size" }
 
-    validates :name, presence: true, length: {maximum: 14, minimum: 2}
-    validates :email, presence: true,uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ ,
-        message: "only allows letters and Uniq email address" }
-       
+    # validates :email, presence: true,uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ ,
+    #      message: "only allows letters and Uniq email address" }
        
     #  validates :name, length: { minimum: 2 }
-    validates :roll, presence: true,uniqueness: true,numericality: { only_integer: true ,message:"Don't again use this roll number"}
-    validates :address, presence: true, length: {maximum: 50, minimum: 5}
+    # validates :roll, presence: true,uniqueness: true,numericality: { only_integer: true ,message:"Don't again use this roll number"}
+    # validates :address, presence: true, length: {maximum: 50, minimum: 5}
 
+
+    # custom validation
+    
     
 
     
 end
+
+
